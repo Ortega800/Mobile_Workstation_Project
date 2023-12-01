2d44af7fce9616e5654fb84474b4db800e0ba7de
Estevan_Ortega <eortega1999@yahoo.com>
2023-12-01 14:07:32 -0500

    Tweaked the topic mapping of the command velocity topic

--- src/mws_robot/launch/mws_robot.launch.py
+++ src/mws_robot/launch/mws_robot.launch.py
@@ -55,6 +55,9 @@ def generate_launch_description():
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
+	remappings=[
+	    ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+	],
     )
 
     joint_state_broadcaster_spawner = Node(
